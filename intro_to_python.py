868c74991351d0be9ee23b888761fa5235518dd4
maikyy24 <108490561+maikyy24@users.noreply.github.com>
2023-01-20 13:05:04 -0500

    Add files via upload

---
+++ intro_to_python.py
@@ -0,0 +1,23 @@
+import sys
+
+i = int(3)
+
+j = int(3)
+
+for x in range(i) :
+  for y in range(j): 
+    sys.stdout.write("#1 ")
+  print("")
+print("")
+for x in range(i) :
+  for y in range(j): 
+    sys.stdout.write("#2 ")
+  print("")
+
+print("")
+i = 3
+j = 2
+for x in range(i) :
+  for y in range(j): 
+    sys.stdout.write("#3 ")
+  print("")
